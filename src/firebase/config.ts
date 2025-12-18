57c870cfe309ddcf471f46b69665809ab747a784
Welinson Silva <1jwbs9@gmail.com>
2025-12-18 20:03:32 +0000

    Apareceu esse erro ao enviar o PDF ou imagens,; ## Error Type Console Fi

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-452861249-230c0",
   "appId": "1:115791268028:web:200f533f239b1e69ba68b9",
+  "storageBucket": "studio-452861249-230c0.appspot.com",
   "apiKey": "AIzaSyBIU22YHUf9FpRZDFQJsr1aD-SzY7e7U8s",
   "authDomain": "studio-452861249-230c0.firebaseapp.com",
   "measurementId": "",
