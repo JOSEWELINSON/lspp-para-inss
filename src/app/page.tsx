36c34027f10a294e3ebfdf237eae3a98fad40b3f
Welinson Silva <1jwbs9@gmail.com>
2025-12-19 12:02:44 +0000

    Continua do mesmo jeito,

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import { InstallPWAButton } from "@/components/InstallPWAButton";
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full items-center justify-center bg-background p-4">
-      <div className="relative w-full max-w-md">
-        <UserLoginForm />
-      </div>
+      <UserLoginForm />
       <div className="fixed bottom-6 right-6 z-50">
         <InstallPWAButton />
       </div>
