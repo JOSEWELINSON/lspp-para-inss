8708b7b19f678788d0a9f112c596070e7f799e94
Welinson Silva <1jwbs9@gmail.com>
2025-12-19 12:23:28 +0000

    Tá certo agora coloque a imagem para instalar o aplicativo: https://fire

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 'use client';
 
 import { useEffect } from 'react';
-import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
